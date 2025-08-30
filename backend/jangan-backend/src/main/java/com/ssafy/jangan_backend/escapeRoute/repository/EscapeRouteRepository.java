92190105b880d18e6a4ad648e825b6729695a7f9
gmltn0907 <gmltn0907@naver.com>
2025-08-30 11:47:21 +0900

    REMOVE: mobile 브랜치 구성을 위해 mobile/ 외 폴더 전체 제거

--- backend/jangan-backend/src/main/java/com/ssafy/jangan_backend/escapeRoute/repository/EscapeRouteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.jangan_backend.escapeRoute.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ssafy.jangan_backend.escapeRoute.entity.EscapeRoute;
-
-public interface EscapeRouteRepository extends CrudRepository<EscapeRoute, Integer> {
-
-}
